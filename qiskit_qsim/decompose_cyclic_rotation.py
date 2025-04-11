95681baa889cb5d4f216399d1ad1a74b9f5322a0
Shoupu Wan <wanshoupu@gmail.com>
2025-04-11 09:00:31 -0700

    qiskit decompose_cyclic_rotation.py

---
+++ qiskit_qsim/decompose_cyclic_rotation.py
@@ -0,0 +1,30 @@
+import cirq
+import numpy as np
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import Operator
+
+from common.cnot_decompose import permeye, cnot_decompose
+from common.format_matrix import MatrixFormatter
+from matplotlib import pyplot as plt
+from qiskit.circuit.library import UnitaryGate
+
+
+def cyclic_matrix(n, i):
+    indexes = list(range(n))
+    xs = indexes[:i] + np.roll(indexes[i:], 1).tolist()
+    m = permeye(xs)
+    return m
+
+
+if __name__ == '__main__':
+    formatter = MatrixFormatter()
+    m = cyclic_matrix(8, 1)
+    print(formatter.tostr(m))
+    op = Operator(m)
+    gate = UnitaryGate(m)
+    qc = QuantumCircuit(3)
+    qc.append(gate, [0, 1, 2])
+    qc_decomposed = qc.decompose(reps=2)
+
+    qc_decomposed.draw(output='mpl')
+    plt.show()
