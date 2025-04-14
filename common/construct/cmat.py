eae46e046539a4151c4471698d627f2a3d8bc6de
Shoupu Wan <wanshoupu@gmail.com>
2025-04-13 20:33:33 -0700

    add X to cmat.py

--- common/construct/cmat.py
+++ common/construct/cmat.py
@@ -9,18 +9,35 @@ from typing import Tuple, Optional
 import numpy as np
 from attr import dataclass
 from numba.core.typing.npydecl import NdIndex
-from numpy._typing import NDArray
 from numpy.typing import NDArray
+
+X = np.eye(2)[[1, 0]]
 
 
 def coreindexes(m) -> Tuple[int, ...]:
-    s = m.shape
-    assert len(s) == 2, f'Matrix must be 2D array but got {s}.'
-    assert s[0] == s[1], f'Matrix must be square but got {s}.'
-    dimension = s[0]
+    validm(m)
+    dimension = m.shape[0]
     identity = np.eye(dimension)
     # indexes of rows / columns that's not that of an identity
     return tuple(i for i in range(dimension) if not (np.allclose(m[i, :], identity[i]) and np.allclose(m[:, i], identity[i])))
+
+
+def validm(m):
+    s = m.shape
+    if len(s) != 2:
+        raise ValueError(f'Matrix must be 2D array but got {s}.')
+    if s[0] != s[1]:
+        raise ValueError(f'Matrix must be square but got {s}.')
+
+
+def validm2l(m: NDArray):
+    """
+    Validate if m is a 2-level unitary matrix.
+    :param m: input matrix.
+    :return: bool True if m is a 2-level unitary matrix; otherwise False.
+    """
+    count = len(coreindexes(m))
+    return count <= 2
 
 
 @dataclass
@@ -80,12 +97,18 @@ class UnitaryM:
         core = m[np.ix_(indexes)]
         return UnitaryM(dimension, core, tuple(indexes))
 
+    def isid(self) -> bool:
+        return np.allclose(self.matrix, np.eye(2))
+
+    def is2l(self) -> bool:
+        return self.matrix.shape[0] <= 2
+
 
 class CUnitary(UnitaryM):
-    def __init__(self, m: NDArray, controls: Tuple[Optional[bool]]):
+    def __init__(self, m: NDArray, controls: Tuple[Optional[bool], ...]):
         """
         Instantiate a controlled single-qubit unitary matrix.
-        :param m: the 2 x 2 core matrix.
+        :param m: the core matrix.
         :param controls: the control qubit together with the 0(False) and 1 (True) state to actuate the control. There should be exactly one None state which is the target qubit.
         Dimension of the matrix is given by len(controls).
         """
@@ -93,7 +116,7 @@ class CUnitary(UnitaryM):
         self.controls = controls
 
     @staticmethod
-    def core_indexes(controls: Tuple[bool]) -> Tuple[int, int]:
+    def core_indexes(controls: Tuple[Optional[bool], ...]) -> Tuple[int, int]:
         t = controls.index(None)
         bits = list(controls)
         num = lambda: int(''.join('1' if b else '0' for b in bits), 2)
@@ -108,7 +131,6 @@ if __name__ == '__main__':
     from common.utils.format_matrix import MatrixFormatter
     from common.utils.mgen import random_unitary
     import random
-    import numpy as np
 
     random.seed(42)
     np.random.seed(42)
@@ -132,21 +154,16 @@ if __name__ == '__main__':
 
     def _test_CUnitary_init():
         m = random_unitary(2)
-        cu = CUnitary(m, [True, True, None])
+        cu = CUnitary(m, (True, True, None))
         print(formatter.tostr(cu.inflate()))
         assert cu.indexes == (6, 7), f'Core indexes is unexpected {cu.indexes}'
+
+
+    def _test_X():
+        assert np.all(np.equal(X[::-1], np.eye(2)))
 
 
     _test_UnitaryM_init()
     _test_create()
     _test_CUnitary_init()
-
-
-def validm2l(m: NDArray):
-    """
-    Validate if m is a 2-level unitary matrix.
-    :param m: input matrix.
-    :return: bool True if m is a 2-level unitary matrix; otherwise False.
-    """
-    count = len(coreindexes(m))
-    return count <= 2
+    _test_X()
