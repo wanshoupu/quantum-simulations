0bef6cd6e01364c6ed3bf123f5dacd873e289c48
Shoupu Wan <wanshoupu@gmail.com>
2025-04-15 13:16:48 -0700

    implement co-Gray code

--- common/utils/gray.py
+++ common/utils/gray.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Tuple, Optional
+from typing import Iterable, Tuple, Optional, List
 
 import numpy as np
 
@@ -16,6 +16,99 @@ def gray_code(n1, n2):
         bit = n2 & mask
         if result[-1] & mask != bit:
             result.append((result[-1] ^ mask) | bit)
+    return result
+
+
+def cogray_code(ns: Tuple[int, int], ms: Tuple[int, int]) -> tuple[list[int], list[int], list[int], list[int]]:
+    """
+    Find the co-Gray code for ns and ms such that the total length of the gray code chain is minimal.
+    Co-Gray code of a group of N numbers (i1,i2,...) consists of N tuples each begins with the corresponding number in the group and ends on a common number.
+    For our purpose, we cull the last number such that destination consists of two numbers that differ only on one bit.
+    For example, suppose ns = (5,4), ms = (6,4). In binary forms, one co-Gray code is
+    n1 101
+    n2 100
+    m1 110 100
+    m2 100 101
+    Therefore, the co-Gray code is
+    ((5,), (4,), (6,4), (4,5))
+    Another co-Gray path is
+    n1 101
+    n2 100
+    m1 110 111 101
+    m2 100
+    So an alternative co-Gray code could be
+    ((5,), (4,), (6,7,5),(4,))
+    Since the total length of Gray chain in both cases are the same, they are equally acceptable.
+    :param ns: a pair of integers, (n1,n2), ni >= 0, i = 1,2
+    :param ms: a pair of integers, (m1,m2), mi >= 0, i = 1,2
+    :return: the co-Gray code
+    """
+    assert ns[0] != ns[1], f'Clashing numbers are forbidden: ns={ns}.'
+    assert ms[0] != ms[1], f'Clashing numbers are forbidden: ms={ms}.'
+    gs = [ns[0]], [ns[1]], [ms[0]], [ms[1]]
+    diffs, parity = diff_parity_bits(ns, ms)
+    if parity:
+        p = parity[0]
+        diffs.remove(p)
+    else:
+        p = diffs.pop()
+        mask = 1 << p
+        if gs[0][-1] & mask == gs[1][-1] & mask:
+            gs[1].append(gs[1][-1] ^ mask)
+        if gs[2][-1] & mask == gs[3][-1] & mask:
+            gs[3].append(gs[3][-1] ^ mask)
+
+    # find the target bit where g1[-1] and g2[-1] differ and g3[-1] and g4[-1] differ
+    for i in diffs:
+        total = sum((gs[j][-1] >> i) & 1 for j in range(len(gs)))
+        bit = 0 if total < 2 else 1
+        mask = 1 << i
+        for j in range(len(gs)):
+            if (gs[j][-1] >> i) & 1 != bit:
+                gs[j].append(gs[j][-1] ^ mask)
+    return gs
+
+
+def diff_parity_bits(ns: Tuple[int, int], ms: Tuple[int, int]) -> Tuple[List[int], List[int]]:
+    """
+    Parity bit indexes are those where the numbers in each group have differing bit at this index.
+    For example, ns = (5,6), ms = (7,0), the parity bits are [0,1] because if written in binary
+    5: 101
+    6: 110
+    7: 111
+    0: 000
+    Note we adopt the Little endian convention the least significant bit has index zero.
+    :param ns:
+    :param ms:
+    :return: The indexes of the parity bits. If no such bit exists, return empty list
+    """
+    bitlength = max(ns + ms).bit_length()
+    parity = []
+    diffs = []
+    for i in range(bitlength):
+        mask = 1 << i
+        if len({ns[0] & mask, ns[1] & mask}) > 1 and len({ms[0] & mask, ms[1] & mask}) > 1:
+            parity.append(i)
+        if len({ns[0] & mask, ns[1] & mask, ms[0] & mask, ms[1] & mask}) > 1:
+            diffs.append(i)
+    return diffs, parity
+
+
+def differ_bits(ns: Tuple[int, int], ms: Tuple[int, int]) -> List[int]:
+    """
+    Find any index of bit where the numbers in each of ns and ms differ on this bit.
+    For example, ns = (5,6), ms = (7,0), the parity bit is 0.
+    Note we adopt the Little endian convention the least significant bit has index zero.
+    :param ns:
+    :param ms:
+    :return: index of the parity bit. If no such bit exists, return None
+    """
+    bitlength = max(ns + ms).bit_length()
+    result = []
+    for i in range(bitlength):
+        mask = 1 << i
+        if len({ns[0] & mask, ns[1] & mask, ms[0] & mask, ms[1] & mask}) > 1:
+            result.append(i)
     return result
 
 
--- tests/common/utils/test_gray.py
+++ tests/common/utils/test_gray.py
@@ -1,8 +1,10 @@
+from typing import Iterable
+
 from common.utils.format_matrix import MatrixFormatter
 import random
 import numpy as np
 
-from common.utils.gray import gray_code, control_bits
+from common.utils.gray import gray_code, control_bits, cogray_code
 
 random.seed(5)
 formatter = MatrixFormatter()
@@ -32,3 +34,30 @@ def test_control_bits():
         # print(bitmatrix)
         expected = [bool(int(bitmatrix[0, i])) if len(set(bitmatrix[:, i])) == 1 else None for i in range(blength)]
         assert gcb == tuple(expected), f'gcb {gcb} != expected {expected}'
+
+
+def assert_gray(gc: Iterable):
+    for m, n in zip(gc, gc[1:]):
+        assert bin(m ^ n).count('1') == 1
+
+
+def test_cogray_code():
+    for _ in range(10):
+        nums = list(range(100))
+        ns = random.sample(nums, k=2)
+        ms = random.sample(nums, k=2)
+        # print(f'ns={ns}, ms={ms}')
+        cgc = cogray_code(ns, ms)
+        # print(cgc)
+        for gc in cgc:
+            assert_gray(gc)
+
+        blength = max(ns + ms).bit_length()
+        mybin = lambda x: bin(x)[2:].zfill(blength)
+        bits = np.array([list(mybin(c[-1])) for c in cgc])
+        # print(bits)
+        bitdiff = [len(set(np.squeeze(bits[:, i]))) > 1 for i in range(bits.shape[1])]
+        assert 1 == sum(bitdiff)
+        i = bitdiff.index(True)
+        assert len(set(np.squeeze(bits[:2, i]))) == 2, f'bits violates parity: {np.squeeze(bits[:2, i])}'
+        assert len(set(np.squeeze(bits[2:, i]))) == 2, f'bits violates parity: {np.squeeze(bits[2:, i])}'
