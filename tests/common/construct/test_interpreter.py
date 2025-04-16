6289f0eb8bf72542b3d9f7d8a281b551d988fe23
Shoupu Wan <wanshoupu@gmail.com>
2025-04-16 12:51:16 -0700

    fix compilation issue in test_interpreter.py

--- tests/common/construct/test_interpreter.py
+++ tests/common/construct/test_interpreter.py
@@ -1,13 +1,15 @@
-from common.construct.circuit import CirqBuilder
+from common.construct.cirq_circuit import CirqBuilder
 from common.construct.interpreter import CircuitInterp
 from common.construct.quompiler import quompile
 from common.utils.mgen import cyclic_matrix
 
 
 def test_interp():
-    u = cyclic_matrix(8, 1)
+    n = 3
+    dim = 1 << n
+    u = cyclic_matrix(dim, 1)
     bc = quompile(u)
 
-    builder = CirqBuilder()
+    builder = CirqBuilder(n)
     interpreter = CircuitInterp(builder)
     interpreter.interpret(bc)
