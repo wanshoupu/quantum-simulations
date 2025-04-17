a058de824633bb2c725e74e9613f1929a8a07954
Shoupu Wan <wanshoupu@gmail.com>
2025-04-16 17:46:14 -0700

    slight presentation changes

--- cirq_qsim/compile/compile_unitary_demo.py
+++ cirq_qsim/compile/compile_unitary_demo.py
@@ -3,10 +3,13 @@ import random
 from common.circuits.cirq_circuit import CirqBuilder
 from common.circuits.interpreter import CircuitInterp
 from common.construct.quompiler import quompile
+from common.utils.format_matrix import MatrixFormatter
 from common.utils.mgen import random_unitary
+import numpy as np
 
 if __name__ == '__main__':
     random.seed(0)
+    np.random.seed(0)
 
     n = random.randint(1, 4)
     dim = 1 << n
@@ -18,5 +21,7 @@ if __name__ == '__main__':
     interpreter.interpret(bc)
     circuit = builder.finish()
     print(circuit)
-    for m in circuit.moments:
+    moments = circuit.moments
+    for m in moments:
         print(m)
+    print(f'Total {len(moments)} moments in the circuit.')
