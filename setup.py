2893151919442add6b963ffd8041964d33e7d4d5
Shoupu Wan <wanshoupu@gmail.com>
2025-04-16 20:36:28 -0700

    fix the package name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="your_package",
+    name="quompiler",
     version="0.1.0",
     packages=find_packages(),
     install_requires=requirements,
