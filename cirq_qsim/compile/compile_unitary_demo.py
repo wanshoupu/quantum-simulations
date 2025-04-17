01c8b6416cbe765fa521efd42054c23f0fadfd69
Shoupu Wan <wanshoupu@gmail.com>
2025-04-16 18:16:16 -0700

    add builtin optimizer to my compile_unitary_demo.py

--- cirq_qsim/compile/compile_unitary_demo.py
+++ cirq_qsim/compile/compile_unitary_demo.py
@@ -1,11 +1,21 @@
 import random
 
+import numpy as np
+from cirq import Circuit, merge_single_qubit_gates_to_phased_x_and_z, eject_z, drop_negligible_operations, drop_empty_moments
+
 from common.circuits.cirq_circuit import CirqBuilder
 from common.circuits.interpreter import CircuitInterp
 from common.construct.quompiler import quompile
-from common.utils.format_matrix import MatrixFormatter
 from common.utils.mgen import random_unitary
-import numpy as np
+
+
+def optimize(circuit: Circuit):
+    circuit = merge_single_qubit_gates_to_phased_x_and_z(circuit)
+    circuit = eject_z(circuit)
+    circuit = drop_negligible_operations(circuit)
+    circuit = drop_empty_moments(circuit)
+    return circuit
+
 
 if __name__ == '__main__':
     random.seed(0)
@@ -20,6 +30,7 @@ if __name__ == '__main__':
     interpreter = CircuitInterp(builder)
     interpreter.interpret(bc)
     circuit = builder.finish()
+    circuit = optimize(circuit)
     print(circuit)
     moments = circuit.moments
     for m in moments:
