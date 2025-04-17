1638cfd7c61693f26a9c25b16259e3399160a910
Shoupu Wan <wanshoupu@gmail.com>
2025-04-16 20:43:29 -0700

    minor changes in compile_cyclic_matrix_demo.py

--- demo/compile_cyclic_matrix_demo.py
+++ demo/compile_cyclic_matrix_demo.py
@@ -1,12 +1,15 @@
 from common.circuits.cirq_circuit import CirqBuilder
 from common.circuits.interpreter import CircuitInterp
 from common.construct.quompiler import quompile
+from common.utils.format_matrix import MatrixFormatter
 from common.utils.mgen import cyclic_matrix
 
 if __name__ == '__main__':
+    formatter = MatrixFormatter(precision=2)
     n = 3
     dim = 1 << n
     u = cyclic_matrix(dim, 1)
+    print(formatter.tostr(u))
     bc = quompile(u)
 
     builder = CirqBuilder(n)
