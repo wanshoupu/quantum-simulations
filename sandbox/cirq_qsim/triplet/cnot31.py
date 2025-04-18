ba7dc8242fb1d2609d3fcd154dbc30f32f021a88
Shoupu Wan <wanshoupu@gmail.com>
2025-04-17 22:16:13 -0700

    restore cnot31.py

--- sandbox/cirq_qsim/triplet/cnot31.py
+++ sandbox/cirq_qsim/triplet/cnot31.py
@@ -1,24 +1,17 @@
 import cirq
-from cirq import CNotPowGate, CXPowGate
 
 from quompiler.utils.format_matrix import MatrixFormatter
 
-
-def cirq_mat():
-    gate = cirq.CXPowGate(exponent=1)
-
-    # Get the unitary matrix
-    matrix = cirq.unitary(gate)
-    print(matrix)
-
-
 if __name__ == '__main__':
-    # 2-qubit unitary matrix: shape (4, 4)
-    import numpy as np
-    U2 = np.eye(4)  # identity for example
+    # Define 3 qubits
+    q0, q1, q2 = cirq.LineQubit.range(3)
 
-    gate2 = cirq.MatrixGate(U2)
-    q0, q1 = cirq.LineQubit.range(2)
-    circuit = cirq.Circuit(gate2(q0, q1))
+    # Create a circuit with an operation only on q0 and q1
+    circuit = cirq.Circuit(
+        cirq.CX(q2, q0)
+    )
+    formatter = MatrixFormatter()
+    print(formatter.mformat(circuit.unitary(qubit_order=[q0, q1, q2])))
 
-    print(circuit)
+    # Explicitly specify all qubits when printing
+    print(circuit.to_text_diagram(qubit_order=[q0, q1, q2]))
