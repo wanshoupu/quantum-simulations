eb486ce99e2016e3666364f6877ca9e26fc0f4fe
Shoupu Wan <wanshoupu@gmail.com>
2025-04-22 23:01:40 -0700

    refactor sym_gen.py with shortened symbols

--- sandbox/sym/sym_gen.py
+++ sandbox/sym/sym_gen.py
@@ -7,25 +7,38 @@ alphabeta = list(string.ascii_lowercase)
 alphamax = len(alphabeta)
 
 
-def square_m(n, prefix: str = None):
+def symmat(n, prefix: str = None):
+    """
+    Create a square symbolic matrix of dimension n x n.
+    When n*n <= 26, single Latin alphabets will be used for symbols.
+    When n <= 26, double Latin alphabets will be used for symbols.
+    Otherwise, symbols of the format '{prefix}:m(:n)', such as, 'a₀₁', 'b₁₁', 'c₁₂', will be used.
+    :param n: dimension
+    :param prefix: optional prefix of the symbols used. When n*n <= 26, prefix is not used because single letter will be automatically used.
+    :return: a matrix with symbols.
+    """
     prefix = prefix or 'a'
 
     if n * n <= alphamax:
-        return square_26(n)
+        return symmat_26(n)
     if n <= alphamax:
-        return square_26_26(n)
-    syms = symbols(f'{prefix}:{n}(:{n})')
-    return Matrix(n, n, syms[:n * n], complex=True)
+        return symmat_26_26(n)
+    return symmat_sub(n, prefix)
 
 
-def square_26(n):
+def symmat_26(n):
     assert n * n <= alphamax
     syms = symbols(' '.join(alphabeta))
     return Matrix(n, n, syms[:n * n], complex=True)
 
 
-def square_26_26(n):
+def symmat_26_26(n):
     assert n <= alphamax
     names = [f"{s}{t}" for s, t in product(alphabeta, repeat=2)]
     syms = symbols(names)
+    return Matrix(n, n, syms[:n * n], complex=True)
+
+
+def symmat_sub(n, prefix):
+    syms = symbols(f'{prefix}:{n}(:{n})')
     return Matrix(n, n, syms[:n * n], complex=True)
