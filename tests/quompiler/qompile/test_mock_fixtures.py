1f9e0b66c0c9cf428c6dcfe0883a67a2e6f0acee
Shoupu Wan <wanshoupu@gmail.com>
2025-05-11 08:05:26 -0700

    Check in unit tests test_mock_fixtures.py

--- tests/quompiler/qompile/test_mock_fixtures.py
+++ tests/quompiler/qompile/test_mock_fixtures.py
@@ -2,8 +2,8 @@ from tests.quompiler.qompile.mock_fixtures import mock_config
 
 
 def test_mock_config(mocker):
-    config = mock_config(mocker, 3)
+    config = mock_config(mocker, 3, emit="SINGLET")
     assert config.target == "CIRQ"
-    assert config.emit == "CLIFFORD_T"
+    assert config.emit == "SINGLET"
     device = config.device
     assert device.arange == [100, 200]
