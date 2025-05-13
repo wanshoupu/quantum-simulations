d8f23f14f05bb9f08b0736a98747c06313c4bfd6
Shoupu Wan <wanshoupu@gmail.com>
2025-05-13 09:51:09 -0700

    Create / finish a config manager with tests

---
+++ quompiler/config/config_manager.py
@@ -0,0 +1,110 @@
+import argparse
+import json
+import os
+
+from numpy.typing import NDArray
+
+from quompiler.config.construct import QompilerConfig, validate_config
+from quompiler.config.numpy_io import load_ndarray
+
+
+class ConfigManager:
+    """
+    Manages the parsing, merging, and updating of configurations.
+    ConfigManager pulls configuration information from three sources:
+    1. Default configurations in config/default_config.json
+    2. Custom configurations user provides through a JSON file
+    3. Custom configurations user provides through command line argument
+    """
+
+    def __init__(self):
+        self._default_config_file = os.path.join(os.path.dirname(__file__), "data", "default_config.json")
+        self._parser = self.create_parser()
+        self._config = json.load(open(self._default_config_file))
+        self._source = None
+
+    def create_config(self) -> QompilerConfig:
+        validate_config(self._config)
+        return QompilerConfig.from_dict(self._config)
+
+    def has_source(self, source: str) -> bool:
+        return bool(source)
+
+    def load_source(self) -> NDArray:
+        return load_ndarray(self._source)
+
+    def load_config(self, json_file: str):
+        """
+        Loads a configuration from a JSON file.
+        :param json_file: a path to the JSON configuration file.
+        """
+        config = json.load(open(json_file))
+        self._config = merge_dicts(self._config, config)
+
+    def parse_args(self):
+        args = self._parser.parse_args()
+        if args.source:
+            self._source = args.source
+        if args.config:
+            self.load_config(args.config)
+        qconfig = self._to_config(args)
+        merged = merge_dicts(self._config, qconfig)
+        self._config = merged
+
+    def help(self):
+        return self._parser.format_help()
+
+    def create_parser(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("source", nargs="?", help="The source to parse, if any", default="")
+        parser.add_argument("-c", "--config",
+                            help="The config file name (JSON format). If provided, it will override the default config "
+                                 "but will be overridden by other arguments", default="")
+        parser.add_argument("-o", "--output", help="output file name", default="a.out")
+        parser.add_argument("-O", "--opt", help="optimization level", choices=["0", "1", "2", "3"], default="0")
+        parser.add_argument("-g", "--debug", help="Print debug level info", action="store_true")
+        parser.add_argument("-Wall", help="Enable all commonly used warning messages", action="store_true")
+        parser.add_argument("-Werror", help="Whether or not treat warning as error", action="store_true")
+        parser.add_argument("--target", help="Target quantum computing platform", choices=["CIRQ", "QISKIT", "QUIMB"], default="CIRQ")
+        parser.add_argument("--emit",
+                            help="specifies what kind of output file the compiler should generate after processing the source code.",
+                            choices=["INVALID", "UNITARY", "TWO_LEVEL", "SINGLET", "MULTI_TARGET", "CTRL_PRUNED", "UNIV_GATE", "CLIFFORD_T"],
+                            default="SINGLET")
+        parser.add_argument("--rtol", help="Relative tolerance — allows for proportional error", default="1e-6")
+        parser.add_argument("--atol", help="Absolute tolerance — allows for fixed error", default="1e-9")
+        parser.add_argument("--ancilla_offset", help="The qubit space offset for ancilla qubits", default=100)
+        return parser
+
+    def save(self, file_path):
+        with open(file_path, "w") as f:
+            json.dump(self._config, f, indent=2)
+
+    def _to_config(self, args):
+        return {
+            "source": args.source,
+            "output": args.output,
+            "optimization": f"O{args.opt}",
+            "debug": args.debug,
+            "warnings": {
+                "all": args.Wall,
+                "as_errors": args.Werror
+            },
+            "device": {
+                "ancilla_offset": int(args.ancilla_offset),
+            },
+            "target": args.target,
+            "emit": args.emit,
+            "rtol": float(args.rtol),
+            "atol": float(args.atol),
+        }
+
+
+def merge_dicts(dict1: dict, dict2: dict) -> dict:
+    result = dict1.copy()
+    for key, value in dict2.items():
+        if key in result and isinstance(result[key], dict):
+            assert isinstance(value, dict)
+            result[key] = merge_dicts(result[key], value)
+        else:
+            result[key] = value
+    return result
---
+++ tests/qompiler/config/test_config_manager.py
@@ -0,0 +1,106 @@
+import os
+import sys
+
+import pytest
+
+from quompiler.config.config_manager import ConfigManager, merge_dicts
+
+fpath = os.path.abspath(os.path.join(os.path.dirname(__file__), "data", "test_compiler_config.json"))
+
+
+def test_parse_default_config():
+    parser = ConfigManager()
+    config = parser.create_config()
+    assert config.device is not None
+    assert config.target == "CIRQ"
+    assert config.emit == "SINGLET"
+
+
+@pytest.mark.parametrize("dict1,dict2,expected", [
+    [{"a": 1}, {"b": 2}, {"a": 1, "b": 2}],
+    [{"foo": "bar"}, {"foo": "bar"}, {"foo": "bar"}],
+    [{"a": 1, "b": {"d": 2}}, {"b": {"c": 1}}, {"a": 1, "b": {"d": 2, "c": 1}}],
+])
+def test_merge_dicts(dict1: dict, dict2: dict, expected: dict):
+    actual = merge_dicts(dict1, dict2)
+    assert actual == expected
+
+
+def test_parse_args(monkeypatch):
+    rtol = 1e6
+    atol = 1e9
+    offset = 77
+    target = 'QUIMB'
+    test_args = ['pyfile.py', 'source_file.txt', '--target', target, "--rtol", str(rtol), "--atol", str(atol), "--ancilla_offset", str(offset)]
+    monkeypatch.setattr(sys, "argv", test_args)
+    config_man = ConfigManager()
+
+    # execute
+    config_man.parse_args()
+    actual = config_man.create_config()
+
+    # verify
+    assert actual.target == target
+    assert actual.rtol == rtol
+    assert actual.atol == atol
+    assert actual.device.ancilla_offset == offset
+
+
+def test_save(mocker):
+    test_args = ['--emit', 'UNIV_GATE', "--rtol", "1e-6", "--atol", "1e-9", "--ancilla_offset", "7"]
+    mocker.patch('sys.argv', test_args)
+    config_man = ConfigManager()
+    config_man.parse_args()
+    assert config_man._config
+
+    mock_open = mocker.mock_open()
+    mocker.patch("quompiler.config.config_manager.open", mock_open)
+    mocker.patch("builtins.open", mock_open)
+    file_path = "tmp_config.json"
+
+    # execute
+    config_man.save(file_path)
+
+    # verify
+    mock_open.assert_called_with(file_path, "w")
+    assert mock_open().write.call_count > 0
+
+
+def test_load_config():
+    man = ConfigManager()
+    default = man.create_config()
+
+    # execute
+    man.load_config(fpath)
+
+    config = man.create_config()
+    assert config.target != default.target
+    assert config.emit != default.emit
+
+
+def test_override_order(mocker):
+    # 1 default
+    man = ConfigManager()
+    default = man.create_config()
+
+    # 2 json file config
+    man.load_config(fpath)
+    file_config = man.create_config()
+
+    # 3 cmd line args
+    test_args = ['--emit', 'UNIV_GATE', "--rtol", "1e-6", "--atol", "1e-9", "--ancilla_offset", "10000"]
+    mocker.patch('sys.argv', test_args)
+    man.parse_args()
+    cmd_config = man.create_config()
+
+    # verify
+    default_device = default.device
+    file_device = file_config.device
+    cmd_device = cmd_config.device
+    print('default_device\n', default_device)
+    print('file_device\n', file_device)
+    print('cmd_device\n', cmd_device)
+
+    assert default_device.ancilla_offset != cmd_device.ancilla_offset
+    assert cmd_device.ancilla_offset != file_device.ancilla_offset
+    assert default_device.ancilla_offset != file_device.ancilla_offset
