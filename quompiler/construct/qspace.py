dae4360cca4648758b10c338b22c45fa945e3c32
Shoupu Wan <wanshoupu@gmail.com>
2025-05-20 10:42:54 -0700

    Prepare to get rid of Ancilla and keep qubit with flag.

--- quompiler/construct/qspace.py
+++ quompiler/construct/qspace.py
@@ -8,33 +8,40 @@ class Qubit:
     Represents a single qubit with a non-negative integer ID.
     """
     qid: int
+    ancilla: bool = False
 
     def __post_init__(self):
         assert self.qid >= 0, f'Qubit {self.qid} must be >= 0'
 
     def __repr__(self):
-        return f'q{self.qid}'
+        prefix = 'a' if self.ancilla else 'q'
+        return f'{prefix}{self.qid}'
 
     def __str__(self):
-        return f'q{self.qid}'
+        return repr(self)
 
     def __eq__(self, __value):
         if not isinstance(__value, Qubit):
             return NotImplemented(f'Comparison is undefined for type {type(__value)}')
-        return self.qid == __value.qid
+        return self.qid == __value.qid and self.ancilla == __value.ancilla
 
     def __hash__(self):
-        return hash(self.qid)
+        return hash(self.qid) + hash(self.ancilla)
 
     def __lt__(self, __value):
         if not isinstance(__value, Qubit):
             return NotImplemented(f'Comparison is undefined for type {type(__value)}')
+        if self.ancilla != __value.ancilla:
+            return self.ancilla < __value.ancilla
         return self.qid < __value.qid
 
     def __le__(self, __value):
         if not isinstance(__value, Qubit):
             return NotImplemented(f'Comparison is undefined for type {type(__value)}')
-        return self.qid < __value.qid or self.qid == __value.qid
+        return self < __value or self == __value
+
+    def is_ancilla(self):
+        return self.ancilla
 
     def to_dict(self) -> Dict:
         return asdict(self)
@@ -54,4 +61,3 @@ class Ancilla(Qubit):
 
     def __str__(self):
         return f'a{self.qid}'
-
