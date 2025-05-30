Reimplement gc_decompose with tsim and fix the sign bug: when U is positively traced, use negative sign and when U is negatively traced, use positive sign.

quompiler/utils/group_su2.py
@@ -6,7 +6,7 @@ from quompiler.construct.types import UnivGate
 from quompiler.utils.mfun import herm
 
 
-def gc_decompose(U: NDArray) -> tuple[NDArray, NDArray]:
+def gc_decompose(U: NDArray) -> tuple[float, NDArray, NDArray]:
     """
     Group commutator decomposition. This is exact decomposition with no approximation.
     Given a SU(2) operator, representing rotation R(n, φ), we decompose it into ±V @ W @ V† @ W†,
@@ -22,23 +22,15 @@ def gc_decompose(U: NDArray) -> tuple[NDArray, NDArray]:
 
     W = UnivGate.X.rotation(beta)
     V = UnivGate.Y.rotation(beta)
-    U2 = -V @ W @ herm(V) @ herm(W)
+    U2 = V @ W @ herm(V) @ herm(W)
 
     assert np.isclose(rangle(U2), alpha)
 
-    P = tsim(U, U2)
+    P = tsim(U2, U)
     WA = P @ W @ herm(P)
     VA = P @ V @ herm(P)
-
-    UP = -VA @ WA @ herm(VA) @ herm(WA)
-    if not np.allclose(UP, U):
-        print()
-        print(f'{alpha}, {beta}')
-        print()
-        print(f'{UP}\n{U}')
-        # assert np.allclose(UP, U), f'\n{UP}\n!=\n{U}'
-
-    return VA, WA
+    sign = 1 if np.trace(U) < 0 else -1
+    return sign, VA, WA
 
 
 def tsim(U, V):
@@ -181,5 +173,5 @@ def gphase(u: NDArray) -> np.complex128:
     det = np.linalg.det(u)
     # unit-magnitude complex number (e^{iϕ})
     phase = np.sqrt(det, dtype=np.complex128)
-    sign = np.sign(np.trace(u)) or 1
+    sign = -1 if np.trace(u) < 0 else 1
     return sign * phase

tests/qompiler/utils/test_group_su2.py
@@ -394,7 +394,9 @@ def test_tsim_nonuniqueness(seed):
     np.random.seed(seed)
 
     """
-    Similarity transformation are not unique. p @ u @ p† = q @ u @ q† does not necessarily mean p = q
+    Similarity transformation are not unique. p @ u @ p† = q @ u @ q† does not necessarily mean p = q.
+    In fact, suppose u is a rotation about an axis n. For any rotation, r, about the same axis, let q = p @ r.
+    Then p @ u @ p† = q @ u @ q†.
     """
 
     u = random_unitary(2)
@@ -416,3 +418,50 @@ def test_tsim_nonuniqueness(seed):
 
     assert not np.allclose(p, q)
 
+
+@pytest.mark.parametrize("seed", random.sample(range(1 << 20), 100))
+def test_gc_decompose_positive_trace(seed):
+    """
+    Positively-traced unitary has negative sign.
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+
+    expected = random_su2()
+    expected = expected / gphase(expected)
+    assert np.isclose(np.linalg.det(expected), 1)
+    assert np.isclose(gphase(expected), 1)
+    assert np.trace(expected) > 0
+
+    # print('expected')
+    # print(formatter.tostr(expected))
+    sign, v, w = gc_decompose(expected)
+    actual = sign * v @ w @ herm(v) @ herm(w)
+    assert sign == -1
+    # print('actual')
+    # print(formatter.tostr(actual))
+    assert np.allclose(actual, expected)
+
+
+@pytest.mark.parametrize("seed", random.sample(range(1 << 20), 10))
+def test_gc_decompose_negative_trace(seed):
+    """
+    Negatively-traced unitary has positive sign.
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+
+    expected = random_su2()
+    expected = -expected / gphase(expected)
+    assert np.isclose(np.linalg.det(expected), 1)
+    assert np.isclose(gphase(expected), -1)
+    assert np.trace(expected) < 0
+
+    # print('expected')
+    # print(formatter.tostr(expected))
+    sign, v, w = gc_decompose(expected)
+    actual = sign * v @ w @ herm(v) @ herm(w)
+    assert sign == 1
+    # print('actual')
+    # print(formatter.tostr(actual))
+    assert np.allclose(actual, expected)