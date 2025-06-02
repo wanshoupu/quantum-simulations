aaca75bd089ea7f66cfd888a8aca25b00d8b0dec
Shoupu Wan <wanshoupu@gmail.com>
2025-06-02 16:31:41 -0700

    More Unit tests for code_analyze.py

--- tests/qompiler/optimize/test_code_analyze.py
+++ tests/qompiler/optimize/test_code_analyze.py
@@ -2,6 +2,9 @@ import json
 import random
 
 from quompiler.construct.bytecode import Bytecode
+from quompiler.construct.cgate import CtrlGate
+from quompiler.construct.qspace import Qubit
+from quompiler.construct.types import UnivGate, QType
 from quompiler.optimize.code_analyze import gen_stats
 from quompiler.utils.mgen import random_UnitaryM, random_indexes, random_ctrlgate
 
@@ -10,15 +13,15 @@ def random_code() -> Bytecode:
     n = random.randint(1, 5)
     dim = 1 << n
     indexes = random_indexes(dim, dim)
-    children = [Bytecode(random_ctrlgate(random.randint(3, 5), random.randint(1, 3), 10000)) for _ in range(3)]
+    children = [Bytecode(random_ctrlgate(random.randint(3, 5), random.randint(1, 3), 5)) for _ in range(3)]
+    children.append(Bytecode(CtrlGate(UnivGate.X, [QType.CONTROL1, QType.CONTROL0, QType.TARGET], [Qubit(201, ancilla=True), Qubit(202, ancilla=True), Qubit(0)])))
     root = Bytecode(random_UnitaryM(dim, indexes), children)
     return root
 
 
 def test_gen_stats():
     code = random_code()
-    # Print the statistics
     stats = gen_stats(code)
     sstats = json.dumps(stats)
     print(sstats)
-    assert len(sstats) == 150
+    assert len(sstats) == 100
