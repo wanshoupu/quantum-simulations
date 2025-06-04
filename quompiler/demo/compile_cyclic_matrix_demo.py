905551adb1b0a067f5fefb4dbdd5de6dd9beddff
Shoupu Wan <wanshoupu@hotmail.com>
2025-06-03 21:19:23 -0700

    Use create_config and update README.md

--- quompiler/demo/compile_cyclic_matrix_demo.py
+++ quompiler/demo/compile_cyclic_matrix_demo.py
@@ -1,23 +1,64 @@
+import argparse
+import tempfile
+
+from matplotlib import pyplot as plt
+from qiskit.converters import circuit_to_dag
+
 from quompiler.circuits.qfactory import QFactory
-from quompiler.config.config_manager import ConfigManager
+from quompiler.config.config_manager import create_config
 from quompiler.construct.types import QompilePlatform
+from quompiler.utils.file_io import CODE_FILE_EXT
 from quompiler.utils.format_matrix import MatrixFormatter
 from quompiler.utils.mgen import cyclic_matrix
 
-if __name__ == '__main__':
-    formatter = MatrixFormatter(precision=2)
-    n = 3
+
+def compile_random_unitary(filename):
+    parser = argparse.ArgumentParser(description="Process some input arguments.")
+    parser.add_argument("-i", "--input", type=str, help="Number of qubits. CAUTION: do not set n to big numbers as it scales exponentially.", required=False, default=3)
+    args, unknown = parser.parse_known_args()
+    n = int(args.input)
     dim = 1 << n
     u = cyclic_matrix(dim, 1)
     print(formatter.tostr(u))
-    config_man = ConfigManager().parse_args()
-    factory = QFactory(config_man.create_config())
+    config = create_config(emit="CLIFFORD_T", ancilla_offset=100, output=filename)
+    factory = QFactory(config)
     compiler = factory.get_qompiler()
-    render = factory.get_render(QompilePlatform.CIRQ)
-
-    # execute
     compiler.compile(u)
 
+
+def render_cirq(filename):
+    config = create_config(target="CIRQ", output=filename)
+    factory = QFactory(config)
+    render = factory.get_render(QompilePlatform.CIRQ)
     codefile = factory.get_config().output
     circuit = render.render(codefile)
     print(circuit)
+    moments = circuit.moments
+    # for m in moments:
+    #     print(m)
+    print(f'Total {len(moments)} moments in the circuit.')
+
+
+def render_qiskit(filename):
+    config = create_config(target="QISKIT", output=filename)
+    factory = QFactory(config)
+    render = factory.get_render(QompilePlatform.QISKIT)
+    codefile = factory.get_config().output
+    circuit = render.render(codefile)
+    dag = circuit_to_dag(circuit)
+    layers = list(dag.layers())
+    # for m in layers:
+    #     print(m)
+    print(f'Total {len(layers)} layers in the circuit.')
+    circuit.draw('mpl')
+    plt.savefig("qc_qiskit_sketch.png")
+    plt.show()
+
+
+if __name__ == '__main__':
+    formatter = MatrixFormatter(precision=2)
+
+    with tempfile.NamedTemporaryFile(suffix=CODE_FILE_EXT, mode="w+", delete=True) as tmp:
+        compile_random_unitary(tmp.name)
+        render_cirq(tmp.name)
+        render_qiskit(tmp.name)
--- quompiler/demo/compile_unitary_demo.py
+++ quompiler/demo/compile_unitary_demo.py
@@ -4,7 +4,7 @@ import tempfile
 from qiskit.converters import circuit_to_dag
 
 from quompiler.circuits.qfactory import QFactory
-from quompiler.config.config_manager import ConfigManager, create_config
+from quompiler.config.config_manager import create_config
 from quompiler.construct.types import QompilePlatform
 from quompiler.utils.file_io import CODE_FILE_EXT
 from quompiler.utils.mgen import random_unitary
@@ -17,9 +17,8 @@ def compile_random_unitary(filename):
     n = int(args.input)
     dim = 1 << n
     u = random_unitary(dim)
-    override = {"output": filename}
-    config_man = ConfigManager().merge(override).parse_args()
-    factory = QFactory(config_man.create_config())
+    config = create_config(emit="CLIFFORD_T", ancilla_offset=100, target="CIRQ", output=filename)
+    factory = QFactory(config)
     compiler = factory.get_qompiler()
     compiler.compile(u)
 
@@ -38,8 +37,8 @@ def render_cirq(filename):
 
 
 def render_qiskit(filename):
-    config_man = create_config(emit="CLIFFORD_T", ancilla_offset=100, target="QISKIT", output=filename)
-    factory = QFactory(config_man.create_config())
+    config = create_config(emit="CLIFFORD_T", ancilla_offset=100, target="QISKIT", output=filename)
+    factory = QFactory(config)
     render = factory.get_render(QompilePlatform.QISKIT)
     codefile = factory.get_config().output
     circuit = render.render(codefile)
