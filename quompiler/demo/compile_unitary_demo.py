fdf1dc9b09ff66192b6e9a786d12fd36a55fcae3
Shoupu Wan <wanshoupu@hotmail.com>
2025-06-03 21:45:18 -0700

    Optimize the gates to remove redundant gates.

--- quompiler/demo/compile_unitary_demo.py
+++ quompiler/demo/compile_unitary_demo.py
@@ -1,6 +1,8 @@
 import argparse
 import tempfile
 
+from matplotlib import pyplot as plt
+from qiskit import transpile
 from qiskit.converters import circuit_to_dag
 
 from quompiler.circuits.qfactory import QFactory
@@ -17,14 +19,14 @@ def compile_random_unitary(filename):
     n = int(args.input)
     dim = 1 << n
     u = random_unitary(dim)
-    config = create_config(emit="CLIFFORD_T", ancilla_offset=100, target="CIRQ", output=filename)
+    config = create_config(emit="CLIFFORD_T", ancilla_offset=100, output=filename)
     factory = QFactory(config)
     compiler = factory.get_qompiler()
     compiler.compile(u)
 
 
 def render_cirq(filename):
-    config = create_config(emit="CLIFFORD_T", ancilla_offset=100, target="CIRQ", output=filename)
+    config = create_config(target="CIRQ", output=filename)
     factory = QFactory(config)
     render = factory.get_render(QompilePlatform.CIRQ)
     codefile = factory.get_config().output
@@ -37,17 +39,20 @@ def render_cirq(filename):
 
 
 def render_qiskit(filename):
-    config = create_config(emit="CLIFFORD_T", ancilla_offset=100, target="QISKIT", output=filename)
+    config = create_config(target="QISKIT", output=filename)
     factory = QFactory(config)
     render = factory.get_render(QompilePlatform.QISKIT)
     codefile = factory.get_config().output
     circuit = render.render(codefile)
-    print(circuit)
+    circuit = transpile(circuit, optimization_level=1)
     dag = circuit_to_dag(circuit)
     layers = list(dag.layers())
     # for m in layers:
     #     print(m)
     print(f'Total {len(layers)} layers in the circuit.')
+    circuit.draw('mpl')
+    plt.savefig("qc_qiskit_sketch.pdf")
+    plt.show()
 
 
 if __name__ == '__main__':
