b8a565c83d1a865fbf1644d27d41aca718520796
Shoupu Wan <wanshoupu@hotmail.com>
2025-06-06 08:14:01 -0700

    minor changes

--- quompiler/demo/compile_unitary_demo.py
+++ quompiler/demo/compile_unitary_demo.py
@@ -9,6 +9,7 @@ from quompiler.circuits.qfactory import QFactory
 from quompiler.config.config_manager import create_config
 from quompiler.construct.types import QompilePlatform
 from quompiler.utils.file_io import CODE_FILE_EXT
+from quompiler.utils.format_matrix import MatrixFormatter
 from quompiler.utils.mgen import random_unitary
 
 
@@ -19,6 +20,7 @@ def compile_random_unitary(filename):
     n = int(args.input)
     dim = 1 << n
     u = random_unitary(dim)
+    print(formatter.tostr(u))
     config = create_config(emit="CTRL_PRUNED", ancilla_offset=100, output=filename)
     factory = QFactory(config)
     compiler = factory.get_qompiler()
@@ -31,6 +33,8 @@ def render_cirq(filename):
     render = factory.get_render(QompilePlatform.CIRQ)
     codefile = factory.get_config().output
     circuit = render.render(codefile)
+    from cirq import merge_single_qubit_gates_to_phased_x_and_z
+    circuit = merge_single_qubit_gates_to_phased_x_and_z(circuit)
     print(circuit)
     moments = circuit.moments
     # for m in moments:
@@ -56,6 +60,8 @@ def render_qiskit(filename):
 
 
 if __name__ == '__main__':
+    formatter = MatrixFormatter(precision=2)
+
     with tempfile.NamedTemporaryFile(suffix=CODE_FILE_EXT, mode="w+", delete=True) as tmp:
         compile_random_unitary(tmp.name)
         render_cirq(tmp.name)
