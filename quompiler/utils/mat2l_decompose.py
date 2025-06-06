7428f9e8d3c3b13f5bc130a9890b0253ddc7bd06
Shoupu Wan <wanshoupu@hotmail.com>
2025-06-06 08:21:57 -0700

    minor changes

--- quompiler/utils/mat2l_decompose.py
+++ quompiler/utils/mat2l_decompose.py
@@ -16,28 +16,28 @@ def mat2l_decompose(m: UnitaryM) -> List[UnitaryM]:
     :return: List of 2-level UnitaryM so that the product of them is equal to the original matrix.
     """
     result = []
-    mcopy = np.copy(m.matrix)
-    s = mcopy.shape
+    matrix = m.matrix
+    s = matrix.shape
     for n in range(s[0] - 1):
-        if validm2l(mcopy):
+        if validm2l(matrix):
             break
-        if np.allclose(mcopy[n + 1:, n], 0):
+        if np.allclose(matrix[n + 1:, n], 0):
             continue
         for i in range(n + 1, s[0]):
             # this is weird! I have to use complex to assign complex to it.
             # check if c will end up with identity
-            if np.isclose(mcopy[i, n], 0) and np.isclose(mcopy[n, n], 1):
+            if np.isclose(matrix[i, n], 0) and np.isclose(matrix[n, n], 1):
                 continue
-            den = np.sqrt(np.conj(mcopy[n, n]) * mcopy[n, n] + np.conj(mcopy[i, n]) * mcopy[i, n])
-            c = np.array([[mcopy[n, n] / den, np.conj(mcopy[i, n]) / den],
-                          [mcopy[i, n] / den, -np.conj(mcopy[n, n]) / den]])
+            den = np.sqrt(np.conj(matrix[n, n]) * matrix[n, n] + np.conj(matrix[i, n]) * matrix[i, n])
+            c = np.array([[matrix[n, n] / den, np.conj(matrix[i, n]) / den],
+                          [matrix[i, n] / den, -np.conj(matrix[n, n]) / den]])
             m2l = UnitaryM(m.dimension, (m.core[n], m.core[i]), c)
             result.append(m2l)
-            mcopy = np.conj(m2l.inflate()).T @ mcopy
-    indxs = coreindexes(mcopy)
+            matrix = np.conj(m2l.inflate()).T @ matrix
+    indxs = coreindexes(matrix)
     if not indxs:
         indxs = (0, 1)
-    m2l = mcopy[np.ix_(indxs, indxs)]
+    m2l = matrix[np.ix_(indxs, indxs)]
     if not np.allclose(m2l, np.eye(2)):
         result.append(UnitaryM(s[0], indxs, m2l))
     return result
