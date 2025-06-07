015862553328d2880905f80c16c134b2990edc1d
Shoupu Wan <wanshoupu@hotmail.com>
2025-06-07 09:47:13 -0700

    Fix error from last refactor in mfun.py

--- quompiler/utils/mfun.py
+++ quompiler/utils/mfun.py
@@ -18,7 +18,21 @@ def herm(m: NDArray) -> NDArray:
     return np.conj(m.T)
 
 
-def allprop(a: NDArray, b: NDArray, rtol=1.e-5, atol=1.e-8, equal_nan=False) -> AllProp:
+@dataclasses.dataclass
+class PropCheck:
+    """
+    This represents the result of a proportionality check.
+    If the check is affirmative, isprop = True and ratio is the porportionality coefficient.
+    Otherwise, isprop = False and ratio is meaningless and should be discarded.
+    """
+    isprop: bool = False
+    ratio: complex = 1.0
+
+    def __bool__(self) -> bool:
+        return self.isprop
+
+
+def allprop(a: NDArray, b: NDArray, rtol=1.e-5, atol=1.e-8, equal_nan=False) -> PropCheck:
     """
     This function tests whether `a` is proportional to `b` such that a = λb with λ as a scalar.
     If the test results in False, ignore the ratio.
@@ -39,7 +53,7 @@ def allprop(a: NDArray, b: NDArray, rtol=1.e-5, atol=1.e-8, equal_nan=False) -> 
     return PropCheck(np.allclose(a, b * ratio, rtol=rtol, atol=atol, equal_nan=equal_nan), ratio)
 
 
-def id_prop(a: NDArray, rtol=1.e-5, atol=1.e-8, equal_nan=False) -> AllProp:
+def id_prop(a: NDArray, rtol=1.e-5, atol=1.e-8, equal_nan=False) -> PropCheck:
     """
     This function tests if a is proportional to an identity matrix such that a = λI with λ as a scalar.
     If the test results in False, ignore the ratio.
@@ -55,7 +69,7 @@ def id_prop(a: NDArray, rtol=1.e-5, atol=1.e-8, equal_nan=False) -> AllProp:
     return allprop(a, np.eye(a.shape[0]), rtol=rtol, atol=atol, equal_nan=equal_nan)
 
 
-def unitary_prop(a: NDArray, rtol=1.e-5, atol=1.e-8, equal_nan=False) -> AllProp:
+def unitary_prop(a: NDArray, rtol=1.e-5, atol=1.e-8, equal_nan=False) -> PropCheck:
     """
     This function tests if whether a is proportional to a unitary matrix U, such that a = λU with λ as a scalar.
     If the test results in False, ignore the ratio.
@@ -74,17 +88,3 @@ def unitary_prop(a: NDArray, rtol=1.e-5, atol=1.e-8, equal_nan=False) -> AllProp
     mat = a.conj() @ a.T
     prop = id_prop(mat, rtol=rtol, atol=atol, equal_nan=equal_nan)
     return PropCheck(prop.isprop, np.sqrt(prop.ratio))
-
-
-@dataclasses.dataclass
-class PropCheck:
-    """
-    This represents the result of a proportionality check.
-    If the check is affirmative, isprop = True and ratio is the porportionality coefficient.
-    Otherwise, isprop = False and ratio is meaningless and should be discarded.
-    """
-    isprop: bool = False
-    ratio: complex = 1.0
-
-    def __bool__(self) -> bool:
-        return self.isprop
