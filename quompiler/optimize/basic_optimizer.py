7e839c75dd460235b3bc627f6494501bb6abbbfa
Shoupu Wan <wanshoupu@hotmail.com>
2025-06-11 18:36:48 -0700

    Finish implementing the optimizer basic_optimizer.py

--- quompiler/optimize/basic_optimizer.py
+++ quompiler/optimize/basic_optimizer.py
@@ -1,8 +1,41 @@
+from itertools import chain
+
+import numpy as np
+from pydantic.utils import defaultdict
 from typing_extensions import override
 
-from quompiler.construct.bytecode import Bytecode
-from quompiler.construct.types import OptLevel
+from quompiler.construct.bytecode import Bytecode, BytecodeIter
+from quompiler.construct.cgate import CtrlGate
+from quompiler.construct.qspace import Qubit
+from quompiler.construct.types import OptLevel, UnivGate
 from quompiler.optimize.optimizer import Optimizer
+
+
+def cancel_append(prefix: dict[Qubit, list], node):
+    assert isinstance(node.data, CtrlGate)
+    stacks = [prefix[q] for q in node.data.qspace]
+    predecessors = {s[-1] if s else None for s in stacks}
+    if len(predecessors) > 1 or (None in predecessors):
+        # None or more than one gates meaning the prior gates qspace are not aligned with this one
+        append_node(node, prefix)
+        return
+
+    prior = next(iter(predecessors))
+    product = np.array(prior.data @ node.data)
+    if np.allclose(product, np.eye(product.shape[0])):
+        node.skip = True
+        prior.skip = True
+        # pop prior
+        for stack in stacks:
+            stack.pop()
+    else:
+        append_node(node, prefix)
+
+
+def append_node(node, prefix):
+    for q in node.data.qspace:
+        stack = prefix[q]
+        stack.append(node)
 
 
 class SlidingWindowOptimizer(Optimizer):
@@ -24,5 +57,13 @@ class SlidingWindowOptimizer(Optimizer):
         return OptLevel.O0
 
     @override
-    def optimize(self, code: Bytecode) -> Bytecode:
-        return code
+    def optimize(self, root: Bytecode) -> Bytecode:
+        prefix = defaultdict(list)
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node.is_leaf():
+                cancel_append(prefix, node)
+            # Add children in reverse order so leftmost child is processed first
+            stack.extend(reversed(node.children))
+        return root
--- tests/qompiler/optimize/test_optimizer.py
+++ tests/qompiler/optimize/test_optimizer.py
@@ -1,55 +1,96 @@
 import os
+import tempfile
 from functools import reduce
 
 import numpy as np
 from matplotlib import pyplot as plt
-from qiskit import transpile
-from qiskit.converters import circuit_to_dag
 
 from quompiler.circuits.qfactory import QFactory
 from quompiler.config.config_manager import ConfigManager
-from quompiler.construct.bytecode import BytecodeIter
-from quompiler.construct.types import QompilePlatform
-from quompiler.utils.file_io import read_code
+from quompiler.construct.bytecode import BytecodeIter, Bytecode
+from quompiler.construct.cgate import CtrlGate
+from quompiler.construct.types import QompilePlatform, UnivGate, QType
+from quompiler.utils.file_io import CODE_FILE_EXT
+from quompiler.utils.mgen import random_unitary, random_ctrlgate
 from tests.qompiler.circuits.test_qompiler import formatter
 
 
-def test_optimize():
-    codefile = os.path.abspath(os.path.join(os.path.dirname(__file__), "data", "for_optimize_test.qco"))
-    code = read_code(codefile)
-    nodes_before_opts = [a.data for a in BytecodeIter(code) if a.is_leaf()]
-    assert len(nodes_before_opts) == 534
+def create_palindromic_code():
+    gates = [CtrlGate(UnivGate.X, [QType.TARGET, QType.CONTROL1]),
+             (random_ctrlgate(4, 2)),
+             CtrlGate(UnivGate.X, [QType.TARGET, QType.CONTROL1, QType.CONTROL0]),
+             ]
+    gates = gates + [gates[-1], gates[-2].herm(), gates[-3]]
+    product = reduce(lambda x, y: x @ y, gates)
+    return Bytecode(product, [Bytecode(g) for g in gates])
 
-    config = ConfigManager().merge(dict(optimization='O3')).create_config()
-    factory = QFactory(config)
 
+def test_optimize_palindrome():
+    code = create_palindromic_code()
+    assert np.allclose(np.array(code.data), np.eye(code.data.order()))
+
+    gates_before_opts = [a.data for a in BytecodeIter(code) if a.is_leaf()]
     # debug
     # print_circuit(codefile, factory)
+
+    assert len(gates_before_opts) == 6
+    config = ConfigManager().merge(dict(emit='PRINCIPAL', ancilla_offset=8, optimization='O3')).create_config()
+    factory = QFactory(config)
 
     # execute
     for opt in factory.get_optimizers():
         code = opt.optimize(code)
 
-    nodes_after_opts = [a.data for a in BytecodeIter(code) if a.is_leaf()]
+    nodes_after_opts = [a for a in BytecodeIter(code) if a.is_leaf() and not a.skip]
 
     # verify
-    assert len(nodes_after_opts) == 534
-
-    before_opt = np.array(reduce(lambda a, b: a @ b, nodes_before_opts))
-    after_opt = np.array(reduce(lambda a, b: a @ b, nodes_after_opts))
-    assert before_opt.shape == after_opt.shape
-    assert np.allclose(after_opt, before_opt), f'\ncompiled=\n{formatter.tostr(after_opt)},\ninput=\n{formatter.tostr(before_opt)}'
+    assert len(nodes_after_opts) == 0
 
 
-def print_circuit(codefile, factory):
+def test_optimize_real_compile():
+    # with tempfile.NamedTemporaryFile(suffix=CODE_FILE_EXT, mode="w+b", delete=True) as codefile:
+    codefile = os.path.abspath(os.path.join(os.path.dirname(__file__), "data", "for_optimize_test.qco"))
+    n = 3
+
+    config = ConfigManager().merge(dict(emit='PRINCIPAL', ancilla_offset=n, optimization='O3', output=codefile)).create_config()
+    factory = QFactory(config)
+
+    dim = 1 << n
+    input_mat = random_unitary(dim)
+    compiler = factory.get_qompiler()
+    root = compiler.decompose(input_mat)
+    code = root.children[5]
+    compiler.output(code)
+
+    gates_before_opts = [a.data for a in BytecodeIter(code) if a.is_leaf()]
+    # debug
+    print_circuit(codefile, factory, "before_opt.pdf")
+
+    assert len(gates_before_opts) == 126
+    # execute
+    for opt in factory.get_optimizers():
+        code = opt.optimize(code)
+
+    nodes_after_opts = [a for a in BytecodeIter(code) if a.is_leaf() and not a.skip]
+
+    # verify
+    assert len(nodes_after_opts) == 108
+
+    compiler.output(code)
+    print_circuit(codefile, factory, "after_opt.pdf")
+
+    gates_after_opts = [a.data for a in nodes_after_opts]
+    expected = np.array(reduce(lambda a, b: a @ b, gates_before_opts))
+    actual = np.array(reduce(lambda a, b: a @ b, gates_after_opts))
+    assert actual.shape == expected.shape
+    assert np.allclose(actual, expected), f'\noptimized=\n{formatter.tostr(actual)},\nexpected=\n{formatter.tostr(expected)}'
+
+
+def print_circuit(codefile, factory, filepath="qc_qiskit_sketch.pdf"):
     render = factory.get_render(QompilePlatform.QISKIT)
     circuit = render.render(codefile)
 
     # Qiskit built-in optimization
-    circuit = transpile(circuit, optimization_level=1)
-
-    dag = circuit_to_dag(circuit)
-    layers = list(dag.layers())
-    assert len(layers) == 304
     circuit.draw('mpl')
-    plt.savefig("qc_qiskit_sketch.pdf", bbox_inches='tight')
+    plt.savefig(filepath, bbox_inches='tight')
+    # plt.show()
